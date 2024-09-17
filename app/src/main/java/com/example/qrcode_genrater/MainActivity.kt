a6b8f92f546ecccbe3bc62ec751a23c47066af8d
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-09-17 13:19:48 +0500

    Model Drawer Sheet add

--- app/src/main/java/com/example/qrcode_genrater/MainActivity.kt
+++ app/src/main/java/com/example/qrcode_genrater/MainActivity.kt
@@ -24,49 +24,50 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.BlurLinear
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material.icons.outlined.Save
 import androidx.compose.material.icons.outlined.Share
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.FileProvider
@@ -75,6 +76,7 @@ import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.qrcode.QRCodeWriter
 import com.lightspark.composeqr.QrCodeView
+import kotlinx.coroutines.launch
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStream
@@ -108,6 +110,7 @@ fun SelectImageFromGallery(onImageSelected: (Uri) -> Unit) {
     }
 }
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -116,127 +119,161 @@ fun GalleryQrCodeApp() {
     var qrCodeBitmap by remember { mutableStateOf<Bitmap?>(null) }
     val context = LocalContext.current
 
-    Scaffold(topBar = {
-        TopAppBar(title = {
-            Text(
-                text = "Create",
-                color = Color.White,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.SemiBold
-            )
-        }, navigationIcon = {
-            Icon(imageVector = Icons.Default.Menu, contentDescription = "", tint = Color.White)
-        }, actions = {
-            Icon(
-                imageVector = Icons.Outlined.MoreVert,
-                contentDescription = "",
-                tint = Color.White
-            )
-        }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Blue)
-        )
-    }) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-                .padding(top = it.calculateTopPadding()),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            if (selectedImageUri == null) {
-                SelectImageFromGallery { uri ->
-                    selectedImageUri = uri
-                }
-            } else {
-                Spacer(modifier = Modifier.height(97.dp))
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-                qrCodeBitmap = generateQRCode(selectedImageUri.toString())
 
-                qrCodeBitmap?.let { bitmap ->
-                    QrCodeView(
-                        data = selectedImageUri.toString(),
-                        modifier = Modifier
-                            .size(300.dp)
-                            .background(Color.White, shape = RoundedCornerShape(8.dp))
-                            .padding(8.dp)
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(modifier = Modifier
+                .requiredWidth(300.dp)
+                .fillMaxHeight()) {
+                Text("Drawer title", modifier = Modifier.padding(16.dp))
+                Divider()
+                NavigationDrawerItem(
+                    label = { Text(text = "Drawer Item") },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
+            }
+        }
+
+    ) {
+        Scaffold(topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Create",
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold
                     )
-                    Spacer(modifier = Modifier.height(40.dp))
-
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 10.dp, end = 10.dp)
-                            .height(55.dp)
-                            .background(Color.LightGray.copy(alpha = 0.20f)),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Start
-                    ) {
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch { drawerState.open() }
+                    }) {
                         Icon(
-                            imageVector = Icons.Outlined.Save,
-                            contentDescription = "",
-                            modifier = Modifier
-                                .size(40.dp)
-                                .clickable {
-                                    saveQRCodeToGallery(context, bitmap)
-                                }
-                        )
-                        Spacer(modifier = Modifier.width(22.dp))
-
-                        Icon(
-                            imageVector = Icons.Outlined.Share,
-                            contentDescription = "",
-                            modifier = Modifier
-                                .size(40.dp)
-                                .clickable {
-                                    shareQRCode(context, bitmap)
-                                }
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Menu",
+                            tint = Color.White
                         )
                     }
-                }
-
-                Spacer(modifier = Modifier.height(22.dp))
-
-                SelectionContainer {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier
-                            .border(
-                                BorderStroke(1.dp, Color.Gray), shape = RoundedCornerShape(8.dp)
-                            )
-                            .padding(start = 16.dp, end = 16.dp)
-                            .background(Color(0xFFFAFAFA))
-                    ) {
-                        Text(
-                            text = "Image URL",
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 16.sp,
-                            color = Color.Black
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            text = selectedImageUri.toString(),
-                            fontSize = 14.sp,
-                            color = Color.Black
-                        )
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Button(
-                    onClick = { selectedImageUri = null },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Red, contentColor = Color.White
-                    ),
-                    modifier = Modifier.size(180.dp, 50.dp),
-                ) {
+                },
+                actions = {
                     Icon(
-                        imageVector = Icons.Default.Delete,
-                        contentDescription = "Remove Image",
-                        modifier = Modifier.size(24.dp)
+                        imageVector = Icons.Outlined.MoreVert,
+                        contentDescription = "",
+                        tint = Color.White
                     )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = "Remove Image")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Blue)
+            )
+        }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .padding(top = it.calculateTopPadding()),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                if (selectedImageUri == null) {
+                    SelectImageFromGallery { uri ->
+                        selectedImageUri = uri
+                    }
+                } else {
+                    Spacer(modifier = Modifier.height(97.dp))
+
+                    qrCodeBitmap = generateQRCode(selectedImageUri.toString())
+
+                    qrCodeBitmap?.let { bitmap ->
+                        QrCodeView(
+                            data = selectedImageUri.toString(),
+                            modifier = Modifier
+                                .size(300.dp)
+                                .background(Color.White, shape = RoundedCornerShape(8.dp))
+                                .padding(8.dp)
+                        )
+                        Spacer(modifier = Modifier.height(40.dp))
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 10.dp, end = 10.dp)
+                                .height(55.dp)
+                                .background(Color.LightGray.copy(alpha = 0.20f)),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Start
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Save,
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(40.dp)
+                                    .clickable {
+                                        saveQRCodeToGallery(context, bitmap)
+                                    }
+                            )
+                            Spacer(modifier = Modifier.width(22.dp))
+
+                            Icon(
+                                imageVector = Icons.Outlined.Share,
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(40.dp)
+                                    .clickable {
+                                        shareQRCode(context, bitmap)
+                                    }
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(22.dp))
+
+                    SelectionContainer {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .border(
+                                    BorderStroke(1.dp, Color.Gray), shape = RoundedCornerShape(8.dp)
+                                )
+                                .padding(start = 16.dp, end = 16.dp)
+                                .background(Color(0xFFFAFAFA))
+                        ) {
+                            Text(
+                                text = "Image URL",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                                color = Color.Black
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = selectedImageUri.toString(),
+                                fontSize = 14.sp,
+                                color = Color.Black
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        onClick = { selectedImageUri = null },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Red, contentColor = Color.White
+                        ),
+                        modifier = Modifier.size(180.dp, 50.dp),
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Remove Image",
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = "Remove Image")
+                    }
                 }
             }
         }
